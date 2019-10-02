b29db51e8a912a84700d30530568c6a13331e098
unknown <kevinnzaworski@gmail.com>
2019-10-01 21:51:32 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Form />
+  </div>
+</template>
+
+<script>
+import Form from './components/todo/form.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Form
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+  width: 960px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/todo/form.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <h1>{{ question }}</h1>
+    <p class="errorText" v-if="error"> {{ error }} <p>
+    <form>
+      <input v-model="toDoInput" placeholder="" />
+      <button v-on:click="handleSubmit">Submit</button>
+      <button v-if="toDoArr[0]" v-on:click="clearList">Clear ToDo's</button>
+    </form>
+    <div>
+      <ul>
+        <!-- To do go here.. -->
+        <li v-for="(element, index) in toDoArr" v-bind:key="index">
+          <input type="checkbox" v-model="element.completed" v-on:click="markDone" v-bind:id="index"> {{ element.item }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+  
+<script>
+  export default {
+    data() {
+      return {
+        error: "",
+        question: "What would you like to get done today?",
+        toDoInput: "",
+        toDoArr: []
+      }
+    },
+    created() {
+      let local = localStorage.getItem("toDos");
+      if(local) this.toDoArr = JSON.parse(local); 
+    },
+    methods: {
+      handleSubmit: function(event) {
+        event.preventDefault();
+        if(this.toDoInput === null || this.toDoInput === "") return this.error = "Task cannot be empty!";
+
+        this.toDoArr.push({
+          "item":this.toDoInput,
+          "completed": false 
+        });
+        this.toDoInput = "";
+        this.error = "";
+        this.writeToStorage();
+      },
+      writeToStorage: function() {
+        localStorage.setItem("toDos", JSON.stringify(this.toDoArr)); 
+      },
+      clearList: function(event) {
+        event.preventDefault();
+        localStorage.removeItem("toDos");
+        this.toDoArr = [];
+      },
+      markDone: function(event) {
+        console.log(event.path);
+        this.toDoArr[event.target.id] = {
+          item: this.toDoArr[event.target.id].item,
+          completed : event.path[0].checked
+        };
+
+        this.writeToStorage();
+      }
+    },
+  }
+</script>
+  
+<style> 
+  .errorText {
+    color: red;
+  }
+
+  ul {
+    list-style-type: none;
+  }
+
+  li {
+    margin-left: -43px;
+    padding-bottom: 10px;
+  }
+</style>
